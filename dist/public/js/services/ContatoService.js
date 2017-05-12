fe500198f2b6627c7255c1951e09d752e19642b3
bruno <bruno.luiz88@gmail.com>
2017-05-12 19:27:51 -0300

    Exclusao vendor e Dist

--- dist/public/js/services/ContatoService.js
+++
@@ -1,3 +0,0 @@
-angular.module('mean').factory('Contato',["$resource", function($resource){
-	return $resource('/contatos/:id');
-}]);
--- dist/server.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-var express = require('express');
-var app = require('./config/express')();
-require('./config/passport.js')();
-require('./config/database.js')('mongodb://localhost/mean');
-
-http.createServer(app).listen(app.get('port'), function(){
-	console.log('Express Server executando na porta: ' + app.get('port'));
-});
